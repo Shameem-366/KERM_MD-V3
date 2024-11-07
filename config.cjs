64e6ef1ea6e22d624b3a7a3c6bc21add3a891274
Cyber-366 <githubaccou366@gmail.com>
2024-11-07 12:32:00 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Kerm v3~2qkStR3Q",
+  SESSION_ID: process.env.SESSION_ID || "Kerm v3~KN5UQa0f",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
